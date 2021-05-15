2a067efcc711797aaff7eb8d8e7e3860b02946e6
Valeria <valery0007@gmail.com>
2021-05-15 22:57:18 +0200

    styles opacity

--- women-in-tech/src/App.js
+++ women-in-tech/src/App.js
@@ -10,13 +10,14 @@ function App() {
           <h1>Women in Tech</h1> 
           </header> 
           <div className="avatars">
-             <Avatar id={2} name="Andrea"/>
+          
               <Avatar id={1} name="Andra"/>
               <Avatar id={5} name="Aria"/>
               <Avatar id={3} name="Pepa"/>
                <Avatar id={4} name="Lola"/>
               <Avatar id={6} name="Maria"/>
                <Avatar id={7} name="Nadia"/>
+                  <Avatar id={2} name="Andrea"/>
           </div>
        </div>
     );
